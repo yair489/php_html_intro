a9624b6191fe2bb71dc6a2cd27cf120b1f32e373
yair489 <yairturgeman@gmail.com>
2024-08-12 00:37:48 +0300

    add edit and delete and confirm

---
+++ section 14 delete_data/article.php
@@ -0,0 +1,31 @@
+<?php
+
+require 'includes/database.php';
+require 'includes/article.php';
+
+$conn = getDB();
+
+if (isset($_GET['id'])) {
+    $article = getArticle($conn, $_GET['id']);
+} else {
+    $article = null;
+}
+
+?>
+<?php require 'includes/header.php'; ?>
+
+<?php if ($article === null) : ?>
+    <p>Article not found.</p>
+<?php else : ?>
+
+    <article>
+        <h2><?= htmlspecialchars($article['title']); ?></h2>
+        <p><?= htmlspecialchars($article['content']); ?></p>
+    </article>
+
+    <a href="edit-article.php?id=<?= $article['id']; ?>">Edit</a>
+    <a href="delete-article.php?id=<?= $article['id']; ?>">Delete</a>
+
+<?php endif; ?>
+
+<?php require 'includes/footer.php'; ?>
